0daba260f58f08aa0c214e92b3346d85373cb936
KM <tsnok777@gmail.com>
2022-08-18 14:02:52 +0300

    Post request processing for formdummy app

--- my_site/formdummy/views.py
+++ my_site/formdummy/views.py
@@ -3,6 +3,13 @@ from django.views import View
 
 class formDummyView(View):
     def get(self, request,*args, **kwargs):
-        return render(request,'formdummy/form.html',{'Text':'Hello all',})
+        return render(request,'formdummy/form.html',{'nameForm':'Feedback form',})
 
 # 'https://docs.djangoproject.com/en/3.0/ref/class-based-views/base/'
+    def post(self,request,*args, **kwargs):
+        text = request.POST.get('text')
+        grade = request.POST.get('grade')
+        image = request.FILES.get('image')
+        content = image.read()
+        #rederict
+        return render(request,'formdummy/form.html',{'nameForm':'Feedback form',})
