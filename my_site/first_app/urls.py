6c1e80704c7548d399281a143af5fc0e8256ea0c
KM <tsnok777@gmail.com>
2022-08-19 15:21:46 +0300

    dinamic rout

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -4,5 +4,7 @@ from .import views
 #domain.com/first_app/simple_view
 urlpatterns = [
     path('simple_view', views.ViewMySite,name='MySiteView'),
-    path('sport_view', views.SportView,name='SportView'),
+    path('sports', views.sport_view,name='SportView'),
+    path('finance', views.finance_view,name='SportView'),
+    path('politics', views.politics_view,name='PoliticsView')
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse,HttpResponseNotFound
 from django.template import loader
 from django.shortcuts import render
 
-
+articles = {
+    'sports':'Sport page',
+    'finance':'Finance page',
+    'politics':'Politics page'
+}
 
 def ViewMySite(request):
     try:
@@ -17,11 +21,11 @@ def ViewMySite(request):
     except:
         return HttpResponseNotFound('No page found')
 
-def SportView(request):
-    try:
-        t = loader.get_template('first_app/ViewMySite.html')
-        c = {'foo': 'bar',}
-        return HttpResponse(t.render(c, request), content_type='text/html')
-    except:
-        result =  f'No page found'
-        return HttpResponseNotFound(result)
+def sport_view(request):
+    return HttpResponse(articles['sports'])
+
+def finance_view(request):
+    return HttpResponse(articles['finance'])
+
+def politics_view(request):
+    return HttpResponse(articles['politics'])
