6fc792e8ae51b6152d6caf7ad321c2b0eb2f5b52
KM <tsnok777@gmail.com>
2022-09-02 12:35:06 +0300

    add validator on model Patient

--- my_site/office/models.py
+++ my_site/office/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+
 
 # Create your models here.
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
+    heartrate = models.IntegerField(default=60,validators=[MinValueValidator(50),MaxValueValidator(360)])
+
+    def __str__(self):
+        return f'{self.last_name},{self.first_name} is {self.age} years old.'
