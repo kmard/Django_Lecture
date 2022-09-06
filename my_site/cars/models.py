efcf77d916cf0ae184bc52a251a4358791b4c913
KM <tsnok777@gmail.com>
2022-09-06 13:27:59 +0300

    create form add/delete car

--- my_site/cars/models.py
+++ my_site/cars/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+
+    #pk
+    brand = models.CharField(max_length=30)
+    year = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
+
+    def __str__(self):
+        return f'Car is {self.brand}/{self.year} pk {self.pk}'
+
--- my_site/cars/views.py
+++ my_site/cars/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 def list_car(request):
-    return render(request,'cars/list_car.html',{})
+    all_cars = models.Car.objects.all()
+    context = {'all_cars':all_cars}
+    return render(request,'cars/list_car.html',context=context)
 
 def delete_car(request):
-    return render(request,'cars/delete_car.html',{})
+    if request.POST:
+       pk = request.POST['pk']
+       try:
+          car = models.Car.objects.filter(pk=pk)
+          car.delete()
+          return redirect(reverse('cars:list_car'))
+       except BaseException  as e:
+           print(e.args)
+           return redirect(reverse('cars:list_car'))
+    else:
+       return render(request,'cars/delete_car.html',{})
 
 def add_car(request):
-    return render(request,'cars/add_car.html',{})
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand,year=year)
+
+       #if user submitted new car --> List.html
+        return redirect(reverse('cars:list_car'))
+        # return list_car(request)
+    else:
+        return render(request,'cars/add_car.html',{})
