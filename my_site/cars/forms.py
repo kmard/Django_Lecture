112e30c66dfff691740d100260579157e39219ea
KM <tsnok777@gmail.com>
2022-09-11 11:25:49 +0300

    Connect css file to base page

--- my_site/cars/forms.py
+++ my_site/cars/forms.py
@@ -4,4 +4,5 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label="First name", max_length=100)
     last_name = forms.CharField(label="Last name", max_length=100)
     email = forms.EmailField(label="email")
-    review = forms.CharField(label="Write your review here")
+    review = forms.CharField(label="Write your review here",
+                             widget=forms.Textarea(attrs={'cols': '80', 'rows': '2','class':'myform'}))
