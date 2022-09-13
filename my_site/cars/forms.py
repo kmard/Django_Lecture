57a1aac29e6dd9a2d3f6c63a4bf4c49fae674a93
KM <tsnok777@gmail.com>
2022-09-13 13:04:58 +0300

    customization

--- my_site/cars/forms.py
+++ my_site/cars/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import Review_stars
 
+
 class ReviewForm(forms.Form):
     first_name = forms.CharField(label="First name", max_length=100)
     last_name = forms.CharField(label="Last name", max_length=100)
@@ -13,4 +14,15 @@ class ReviewForm(forms.Form):
 class ReviewFormStars(ModelForm):
     class Meta:
         model = Review_stars
-        fields = ['first_name', 'last_name', 'stars']
+        # fields = ['first_name', 'last_name', 'stars']
+        fields = '__all__'
+        labels={'first_name':'YOUR FIRST NAME',
+                'last_name':'YOUR SECOND NAME',
+                'stars':'RATING'
+        }
+        error_messages={
+            'stars':{
+                'min_value':'Min star is 1',
+                'mix_value': 'Max star is 10',
+            },
+        }
--- my_site/cars/models.py
+++ my_site/cars/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 
 # Create your models here.
@@ -19,7 +20,7 @@ class Car(models.Model):
 class Review_stars(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    stars = models.IntegerField()
+    stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
 
     class Meta:
         verbose_name = "Review"
